069f6528d79f216876b803ea56da847101788f9b
Samuel1209-benkai <samuelsenouvo@gmail.com>
2022-12-02 21:37:32 +0100

    test whith two component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import React ,{useState,useEffect,useRef}from "react";
 import Forms from './component/forms';
+import Table from "./component/table";
 function App() {
+  const [formsData, setFormData] = useState({
+    nom: "",
+    prenom: "",
+    telephone: "",
+});
+const [nameArray, setName] = useState([])
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  setName(prevState => {return [...nameArray , formsData] })
+}
+
+function ordonner(e) {
+  setName([...nameArray].sort((a, b) => a.nom.localeCompare(b.nom)));
+}
+function handlechanged(event) {
+  setFormData(prevState => {
+      return {
+          ...prevState,
+          [event.target.name]: event.target.value,
+      }
+  })
+}
+
   return (
     <div className="App">
-      <Forms/>
+      <Forms submit ={handleSubmit}
+      data ={formsData}
+      changed={handlechanged}/>
+      <Table
+            headerClick={ordonner}
+            tableauDeNom={nameArray} />
     </div>
   );
 }
--- src/component/forms.js
+++ src/component/forms.js
@@ -2,71 +2,46 @@ import React ,{useState,useEffect,useRef}from "react";
 import Table from "./table";
 
 
-function Forms() {
+function Forms(props) {
     const userRef = useRef();
-    const [formsData, setFormData] = useState({
-        nom: "",
-        prenom: "",
-        telephone: "",
-    });
-    const [nameArray, setName] = useState([])
-
-
     useEffect(() => {
         userRef.current.focus();
     }, []);
-    function changed(event) {
-        setFormData(prevState => {
-            return {
-                ...prevState,
-                [event.target.name]: event.target.value,
-            }
-        })
-    }
-    async function handleSubmit(e) {
-        e.preventDefault();
-        setName(prevState => {return [...nameArray , formsData] })
-    }
-    
-    function ordonner(e) {
-        setName([...nameArray].sort((a, b) => a.nom.localeCompare(b.nom)));
-    }
+ 
 
     return (<div className="m-10">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={props.submit}>
             <label>Nom</label>
             <input className=" border border-black m-4 p-3 rounded "
                 type="text"
                 placeholder="nom"
                 ref={userRef}
-                onChange={changed}
+                onChange={props.changed}
                 name="nom"
-                value={formsData.nom}
+                value={props.data.nom}
             />
             <br />
             <label>prenom </label>
             <input className=" border border-black m-4 p-3 rounded "
                 type="text"
                 placeholder="prenom"
-                onChange={changed}
+                onChange={props.changed}
                 name="prenom"
-                value={formsData.prenom}
+                value={props.data.prenom}
             /><br />
             <label>Telephone </label>
             <input className=" border border-black m-4 p-3 rounded "
                 type="text"
                 placeholder="telephone"
-                onChange={changed}
+                onChange={props.changed}
                 name="telephone"
-                value={formsData.telephone}
+                value={props.data.telephone}
             />
             <br />
             <button type="Submit" className="border ml-80 m-4 p-1  px-3  bg-blue-600  text-white rounded-md " > Submit</button>
         </form>
 
-        <Table
-            headerClick={ordonner}
-            tableauDeNom={nameArray} />
+        
     </div>
     )
 }
